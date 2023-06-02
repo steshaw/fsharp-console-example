9ef1a50f8544cec6776a50c795ca5e2a7ee5d3a0
Steven Shaw <steven@steshaw.org>
2023-06-02 20:47:05 +1000

    The meaning of life, the universe, and everything

--- Main.fs
+++ Main.fs
@@ -1,4 +1,9 @@
 ﻿let printGreeting name =
-    printfn $"Hello {name} from F#!"
+    printfn "%s," name
+    printfn "The Answer to the Great Question..."
+    printfn "Of Life, the Universe and Everything..."
+    printfn "Is..."
+    printfn "Is..."
+    printfn "%d" 42
 
-printGreeting "Ana"
+printGreeting "Phouchg"
